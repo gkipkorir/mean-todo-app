5465851bb2f99d33b8a689357e08ab8d534a87ec
Gilbert Kipkorir <kipkorirgilbert@gmail.com>
2016-09-16 18:08:46 -0400

    add autoupdate

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,22 +1,48 @@
+//This is where the controllers live
 export default function($scope){
+	let params = {
+		createHasInput: false
+	}; 
 	$scope.todos = [
 	{
 		task: "do dishes",
-		isCompleted: false
+		isCompleted: false,
+		isEditing: false
 	},
 	{
-		task: "walk the dog",
-		isCompleted: true		
+		task: "walk the do,g",
+		isCompleted: true,
+		isEditing: false
 	}
 	];
+
 	$scope.onCompletedClick = function(todo){
 		console.log("onCompletedClick got called!");
 		todo.isCompleted = !todo.isCompleted;
 	};
+
+	$scope.createTask = function(){
+		console.log("creaTask got called!")
+		params.createHasInput = false;
+		$scope.createTaskInput = '';
+	};
+
+	$scope.onEditClick = function(todo){
+		todo.isEditing = true;
+	};
+
 	$scope.$watch('createTaskInput', function(val){
-		if(val){
+		if(!val && params.createHasInput){
+			$scope.todos.pop();
+			params.createHasInput = false;
+		}
+		else if(val && !params.createHasInput){
 			$scope.todos.push({task: val, isCompleted: false});
+			params.createHasInput = true;
+		}else if(val && params.createHasInput){
+			$scope.todos[$scope.todos.length - 1].task = val; //set the previous todo task to val
 		}
 		
 	});
+
 }
